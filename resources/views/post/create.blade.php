b0e6cb0fc05630b4b8304b34357f5e9540c205e8
Zac <a19860101@gmail.com>
2020-07-15 11:19:32 +0800

    20200715

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,16 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+@extends('template.master')
+@section('page-title')
+新增資料
+@endsection
+@section('main')
     <form action="{{ route('post.store') }}" method="post">
         @csrf
         <input type="text" name="title">
         <textarea name="content" id="" cols="30" rows="10"></textarea>
         <input type="submit" value="新增">
     </form>
-</body>
-</html>
+@endsection
