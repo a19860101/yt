5d24fe2b2665a615dbb33c642d93cc73a853a916
Zac <a19860101@gmail.com>
2020-07-17 15:27:20 +0800

    20200717

--- database/seeds/PostTableSeeder.php
+++ database/seeds/PostTableSeeder.php
@@ -12,11 +12,12 @@ class PostTableSeeder extends Seeder
     public function run()
     {
         //
-        // Post::truncate();
-        Post::create(["title"=>"標題一","content"=>"內文一"]);
-        Post::create(["title"=>"標題二","content"=>"內文二"]);
-        Post::create(["title"=>"標題三","content"=>"內文三"]);
-        Post::create(["title"=>"標題四","content"=>"內文四"]);
-        Post::create(["title"=>"標題伍","content"=>"內文伍"]);
+        Post::truncate();
+        factory(Post::class, 50)->create();
+        // Post::create(["title"=>"標題一","content"=>"內文一"]);
+        // Post::create(["title"=>"標題二","content"=>"內文二"]);
+        // Post::create(["title"=>"標題三","content"=>"內文三"]);
+        // Post::create(["title"=>"標題四","content"=>"內文四"]);
+        // Post::create(["title"=>"標題伍","content"=>"內文伍"]);
     }
 }
